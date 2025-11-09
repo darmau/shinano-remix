73f847b567993c586c4fe363de920fe3997b0e91
李大毛 <github@darmau.co>
2025-11-09 18:20:19 +0900

    Remove unused signup route file for cleaner codebase

--- app/routes/$lang.signup.tsx
+++
@@ -1,9 +0,0 @@
-import { redirect } from "react-router";
-import type { LoaderFunctionArgs } from "react-router";
-
-export const loader = ({params}: LoaderFunctionArgs) => {
-  const lang = (params.lang as string) ?? "zh";
-  return redirect(`/${lang}/login`);
-};
-
-export const action = loader;
